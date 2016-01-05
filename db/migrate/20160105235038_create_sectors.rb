96bf8a31369a4a6d9e14b37bb37bc78462b1779f
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-01-05 19:55:36 -0400

    se Hicieron las migraciones de los schemas falta las relaciones entre los modelos

--- db/migrate/20160105235038_create_sectors.rb
+++ db/migrate/20160105235038_create_sectors.rb
@@ -1,7 +1,8 @@
 class CreateSectors < ActiveRecord::Migration
   def change
     create_table :sectors do |t|
-
+      t.string :name
+      t.integer :city_id
       t.timestamps null: false
     end
   end
