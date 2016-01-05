51d26d52044446cecdef4a3d19df0cc78f2ae91b
Edmundo Pichardo <mundix@Edmundos-MacBook-Pro.local>
2016-01-05 07:08:50 -0400

    Se genero el controller session y algunas cosas mas, solo que el password_digest no se esta procesando aun no se por que, pero acabo de crear otro repo que es basado en rails 3 pero en la 4 y funciona el register

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  get 'git/cool'
+  # get 'sessions/new'
+  #
+  # get 'sessions/create'
+  #
+  # get 'sessions/destroy'
 
-  get 'git/sweet'
+  # get 'git/cool'
+
+  # get 'git/sweet'
 
   # get 'users/new'
   #
   # get 'users/create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-  root to: 'gif#cool'
+  # root to: 'gif#cool'
 
   # I've created a gif controller so I have a page I can secure later.
   # This is optional (as is the root to: above).
@@ -17,7 +27,7 @@ Rails.application.routes.draw do
 
   # These routes will be for signup. The first renders a form in the browse, the second will
   # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
+  get 'signup' => 'users#new'
   post '/users' => 'users#create'
 
 
